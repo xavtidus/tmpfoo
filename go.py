2367657b4851ff0732665a10c84cb7a0d8e5080b
Ubuntu <ubuntu@ip-172-31-41-157.ec2.internal>
2021-03-24 02:20:40 +0000

    fixes

--- go.py
+++ go.py
@@ -9,7 +9,7 @@ ap.add_argument("-w", "--work-dir", required=True, help="the working directory")
 ap.add_argument("-o", "--output-bucket", required=True, help="the output s3 bucket")
 ap.add_argument("-b", "--source-bucket", required=True, help="the source s3 bucket")
 ap.add_argument("-k", "--source-key", required=True, help="the source mp4 key")
-
+args = vars(ap.parse_args())
 os.system("mkdir {}".format(args["work_dir"]))
 
 os.system("python3 01_video_to_frame_utils.py --video_s3_bucket {} --video_s3_key {} --working_directory '{}' --visualize_video True --visualize_sample_rate 1 -o '{}'".format( args["source_bucket"], args["source_key"], args["work_dir"], args["output_bucket"] ))
